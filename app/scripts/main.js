32f0b9f1ed4f8d35e8052ee5efd3cda68b551cb5
Drew Botka <hellodrewbot@gmail.com>
2015-08-20 18:42:04 -0400

    plowing through the layout, added bg image for intro and font mixins

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,36 @@ console.log('js loaded');
 
 $(document).ready(function(){
 	console.log('document ready');
+
 	var cards = $('.card').toArray();
+
 	console.log(cards);
-	for (var i = 0; i < cards.length; i++) {
-		console.log(cards[i]);
+
+	// for (var i = 0; i < cards.length; i++) {
+	// 	console.log(cards[i]);
+
+	// }
+
+	function appendToCard(card) {
+		// $('div.card').text('shiza');
+		// card.text('shiza');
+		console.log(card);
+		return card;
 	}
+
+	cards.forEach(appendToCard);
+
 });
+
+
+// // Image replace based on screen width
+// $(window).resize(function(){
+//     var windowWidth = $(window).width();
+//     var imgSrc = $('#image');
+//     if(windowWidth <= 800){         
+//         imgSrc.attr('src','image1.jpg');
+//     }
+//     else if(windowWidth > 800){
+//         imgSrc.attr('src','image2.jpg');
+//     }
+// });
