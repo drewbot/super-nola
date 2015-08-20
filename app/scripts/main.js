82ce19f9959a74cb86d5da00bc4f4ab072f80ebf
Drew Botka <hellodrewbot@gmail.com>
2015-08-19 23:48:46 -0400

    added cards and started array manipulation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+console.log('js loaded');
+
+$(document).ready(function(){
+	console.log('document ready');
+	var cards = $('.card').toArray();
+	console.log(cards);
+	for (var i = 0; i < cards.length; i++) {
+		console.log(cards[i]);
+	}
+});
